3f207d578d7042e9120f74613d71aea2d4a8997b
Katherine Pryszlak <kepryzk@gmail.com>
2017-10-11 14:22:11 -0700

    init commit 2

---
+++ lab-katherine/sluggram-frontend/src/lib/redux-thunk.js
@@ -0,0 +1,7 @@
+
+export default store => next => action => {
+  console.log('scott was here', action);
+  return typeof action === 'function'
+    ? action(store.dispatch, store.getState)
+    : next(action);
+};
