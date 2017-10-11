1b2a76db28584765d5a8a34287c5c0e49c370a33
Tim Turner <timmyturner@me.com>
2017-10-11 15:34:40 -0700

    adds photo upload to dashboard

--- lab-tim/frontend/src/component/dashboard-container/index.js
+++ lab-tim/frontend/src/component/dashboard-container/index.js
@@ -3,14 +3,54 @@ import {connect} from 'react-redux';
 import * as utils from '../../lib/utils';
 
 class DashboardContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   componentWillMount() {
     this.props.auth ? undefined : this.props.history.replace('/');
+  }
+
+  handleChange(e) {
+    let {type, name} = e.target;
+    if(name === 'photo') {
+      let {files} = e.target;
+      let photo = files[0];
+      this.setState({photo});
+
+      utils.photoToDataUrl(photo)
+        .then(photos => this.setState({photos}))
+        .catch(console.error);
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.onComplete(this.state);
   }
 
   render() {
     return (
       <div className='dashboard-container'>
         <h2>Hello from the Dashboard!</h2>
+        <h3>Upload an image to add to the page.</h3>
+        <form
+          className="photo-form"
+          onSubmit={this.handleSubmit}>
+
+          <input
+            type="file"
+            name="photo"
+            onChange={this.handleChange}/>
+
+          <button type="submit">Submit</button>
+        </form>
+        <img src={this.state.photos} style={{'width': '25%'}}/>
       </div>
     );
   }
