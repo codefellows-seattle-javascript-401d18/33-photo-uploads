dd7b1679db742ddf814e35539b5b910fd3ef095c
Katherine Pryszlak <kepryzk@gmail.com>
2017-10-11 17:48:42 -0700

    photo not going to s3

--- lab-katherine/sluggram-frontend/src/component/dashboard-container/index.js
+++ lab-katherine/sluggram-frontend/src/component/dashboard-container/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import * as utils from '../../lib/utils';
+import PhotoForm from '../photo-form';
+import PhotoItem from '../photo-item';
+import {photoFetchRequest, photoCreateRequest} from '../../action/photo-actions';
+
 
 class DashboardContainer extends React.Component {
+  componentDidMount() {
+    this.props.photoFetch()
+      .then(() => console.log('do i exist', this.props));
+  }
+
   componentWillMount() {
     this.props.auth ? undefined : this.props.history.replace('/');
   }
 
   render() {
     return (
-      <div>
-        <h2>I are Dashboard</h2>
+      <div className="dashboard-container">
+        <div className="photo-form-container">
+          <h2>Create your photo!</h2>
+          <PhotoForm
+            onComplete={(photo) => {
+              return this.props.photoCreate(photo)
+                .catch(console.error)
+            }}/>
+          {utils.renderIf(this.props.photo,
+            this.props.photo.map(photo =>
+              <Col sm={6} md={3}>{
+                <PhotoItem key={photo._id} photo={photo}
+                />
+              }<br/></Col>
+            ))}
+        </div>
+        )
       </div>
     );
   }
@@ -18,8 +42,13 @@ class DashboardContainer extends React.Component {
 
 let mapStateToProps = state => ({
   auth: state.auth,
+  photo: state.photo,
+  profile: state.profile,
 });
 
-let mapDispatchToProps = dispatch => ({});
+let mapDispatchToProps = dispatch => ({
+  photoCreate: photo => dispatch(photoCreateRequest(photo)),
+  photoFetch: () => dispatch(photoFetchRequest()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer);
